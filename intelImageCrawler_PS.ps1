0279331ce1d351a95cc8f63718b2c5b835a50d54
SCIS Security <dchow@xtecsystems.com>
2016-07-08 11:33:51 -0500

    Added support tools for intelligence parsing
    Microsoft API Cognitive Services leveraged for Intel and Data Harvesting

---
+++ intelImageCrawler_PS.ps1
@@ -0,0 +1,82 @@
+﻿<#
+#intelImageCrawlerPS v1.0
+
+Takes an input list of web addresses and then has Microsoft Cognitive
+API's analyze the images for context and spits it back out with the originl
+URI. 
+
+Requirements:
+Powershell v3 or higher and a Microsoft API key for "Computer Vision"
+
+Note: The reason why you have to specify https:// or http:// with the domain
+is because the Microsoft API throws a bad request error if you don't. Instead
+of concatenating the prefix; you never know if it's https or http. So feel
+free to modify.
+
+
+This is licensed under GPLv2 and is free for use with no warranties.
+
+Usage: intelimageCrawlerPS.ps1 -i fileofurls.txt -k apikey -t tempresultsfile  
+
+-i parameter = a list of single line urls such as https://google.com
+-k parameter = api key that you get when you sign up with Microsoft
+-t parameter = file path for temporary file
+
+dennis.chow[AT]scissecurity.com
+www.scissecurity.com
+
+#>
+
+#cli_parameters
+param([string]$i, [string]$k, [string]$t);
+
+#grab into array use .toString per object [string] in front of variable causes all to be one object
+$domainList = Get-Content $i.toString()
+
+
+
+ForEach ($x in $domainList)
+{
+    [string]$caturl = $x + $( Invoke-WebRequest -Uri $x -UserAgent "scissecurity.com" ).Images.src
+    echo $caturl | Out-File -Append $t
+    #Lazy to do event based check. Used sleep instead. Don't hate.
+    Sleep 1
+}
+
+#Begin Text Analytics API Call with Invoke-RestMethod wrapper
+[string]$apiUrlimage = "https://api.projectoxford.ai/vision/v1.0/analyze?visualFeatures=Categories,Tags,Description,Faces,ImageType,Color,Adult"
+
+[string]$apiUrlocr = "https://api.projectoxford.ai/vision/v1.0/ocr?language=unk&detectOrientation=true"
+
+[string]$apiKey = $k
+
+
+$headers = @{ "Ocp-Apim-Subscription-Key" = $apiKey }
+
+#Grab New URL list
+$urlList = Get-Content $t.toString()
+
+ForEach ($z in $urlList)
+{
+    
+    #create json object
+    $imageUri = @{url = $z.toString() };
+    $jsonimg = ConvertTo-Json $imageUri;
+
+
+    $imageResults = Invoke-RestMethod -Method Post -Uri $apiUrlimage -Headers $headers -Body $jsonimg -ContentType "application/json"  -ErrorAction Stop
+    $ocrResults = Invoke-RestMethod -Method Post -Uri $apiUrlocr -Headers $headers -Body $jsonimg -ContentType "application/json"  -ErrorAction Stop
+
+    #One liner `n is equivalent to \n
+    Write-Host `n $z `n
+    Write-Host $imageResults.tags `n $imageReuslts.description `n $imageResults.categories `n $imageResults.adult
+
+    #$ocrResults = Invoke-RestMethod -Method Post -Uri $apiUrlocr -Headers $headers -Body $jsonimg -ContentType "application/json"  -ErrorAction Stop
+    Write-Host `n $ocrResults.regions.lines.words.text
+    
+    #Too lazy to do a Wait-Event item. Don't hate.
+    Sleep 2
+}
+
+#remove temp files
+rm $t
---
+++ intel_tldr_PS.ps1
@@ -0,0 +1,87 @@
+﻿<#
+#inteltldrPS v1.0 - Intelligence Too Lazy Didn't Read
+
+Requirements:
+Powershell v3 or higher and a Microsoft API key for "Text Analytics"
+
+Note: You may get hit or miss results using this API. It requires
+very clearn and scrubbed text without special characters in most
+cases. In our opinion it isn't ready for prime time yet. It also
+refuses to handle larger text files.
+
+This is licensed under GPLv2 and is free for use with no warranties.
+
+Usage: intelimageCrawlerPS.ps1 -i urloftext -k apikey
+
+-i parameter = a list of single line urls such as https://google.com/robots.txt
+-k parameter = api key that you get when you sign up with Microsoft
+
+dennis.chow[AT]scissecurity.com
+www.scissecurity.com
+
+#>
+
+#cli_parameters
+param([string]$i, [string]$k);
+
+
+#Microsoft Cognitive API is picky. Have to have prescrubbed plain text.
+#html tag stripper function
+function htmlStrip ($results)
+	{
+	#using .NET toString method to ensure PS doesn't interpret same var incorrectly
+	$results = $results.toString()
+	$results -replace '<[^>]*(>|$)'
+	}
+
+
+
+<#
+#get input url from UI
+Try
+{
+	#UI is too ugly
+	#$sourceUrl = [string]$Host.ui.ReadLine()
+	[string]$sourceUrl = Read-Host "Enter a URL such as https://foobar.com"
+}
+Catch
+{
+	Write-Host "URL requires http:// or https:// prefix e.g. https://cnn.com"
+}
+#>
+
+
+#New instance .NET client as object
+$webClient = New-Object Net.WebClient
+[string]$results = $webClient.DownloadString($i)
+
+#Call local function htmlStrip and utilize explicit argument else null
+[string]$cleanResults = htmlStrip $results
+
+
+
+#convert to awful JSON format for API required input
+$body = [ordered]@{
+    "documents" = 
+	@(
+        @{ "language" = "en"; "id" = $i; "text" = $cleanResults }
+    )
+}
+
+$jsonBody = $body | ConvertTo-Json
+
+#Begin Text Analytics API Call with Invoke-RestMethod wrapper
+[string]$apiUrlphrases = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases"
+[string]$apiUrlsent = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment"
+[string]$apiKey = $k
+
+
+$headers = @{ "Ocp-Apim-Subscription-Key" = $apiKey }
+
+$resultPhrases = Invoke-RestMethod -Method Post -Uri $apiUrlphrases -Headers $headers -Body $jsonBody -ContentType "application/json"  -ErrorAction Stop
+
+$resultSent = Invoke-RestMethod -Method Post -Uri $apiUrlsent -Headers $headers -Body $jsonBody -ContentType "application/json"  -ErrorAction Stop
+
+#Write-Host "Key Phrases:" $resultPhrases.documents.keyPhrases
+$resultPhrases.documents.KeyPhrases | Group-Object | Sort-Object
+Write-Host "Sentiment Score:"$resultSent.documents.score
